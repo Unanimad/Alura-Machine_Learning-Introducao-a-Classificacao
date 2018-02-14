20f4c863d2541620b9383c05957bb7ecdf537ac4
Raphael F <raphaelf.ti@gmail.com>
2018-02-14 11:55:04 -0300

    Aula 06 - Vídeo 02
    Criada função para treinar e preve
    Implementado algoritmo AdaBoostClassifier

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,8 +1,22 @@
 from collections import Counter
 import pandas as pd # pd = python data or 'pandas'
 
+# Treina e preve
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
+    modelo.fit(treino_dados, treino_marcacoes)
+
+    resultado = modelo.predict(teste_dados)
+
+    acertos = (resultado == teste_marcacoes)
+
+    total_acertos = sum(acertos)  # soma todos os elementos
+    total_elementos = len(teste_dados)
+    taxa_acerto = 100.0 * total_acertos / total_elementos
+
+    print("Taxa de acerto do {0}: {1}".format(nome, taxa_acerto))
+
 # df = data frame
-df = pd.read_csv('dist/busca.csv')
+df = pd.read_csv('dist/busca2.csv')
 X_df = df[['home', 'busca', 'logado']]  # sendo mais de uma, necessario estar num array
 Y_df = df['comprou']
 
@@ -25,22 +39,19 @@ teste_marcacoes = Y[-tamanho_teste:]  # pega as marcacoes para teste
 
 
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
 
 modelo = MultinomialNB()
-modelo.fit(treino_dados, treino_marcacoes)
+fit_and_predict('MultinomialNB', modelo, treino_dados, treino_marcacoes,
+    teste_dados, teste_marcacoes)
 
-resultado = modelo.predict(teste_dados)
-
-acertos = (resultado == teste_marcacoes)
-
-total_acertos = sum(acertos)  # soma todos os elementos
-total_elementos = len(teste_dados)
-taxa_acerto = 100.0 * total_acertos / total_elementos
-
-print("Taxa de acerto do algoritmo: %f" % taxa_acerto)
+modelo = AdaBoostClassifier()
+fit_and_predict('AdaBoostClassifier', modelo, treino_dados, treino_marcacoes,
+    teste_dados, teste_marcacoes)
 
 # Chutar com o elemento mais recorrente
 acerto_base = max(Counter(teste_marcacoes).values())  # retorna o elemento com mais recorrente
 taxa_acerto_base = acerto_base / len(teste_marcacoes) * 100.0
 
-print("Taxa de acerto base: %f" % taxa_acerto_base)
+print('Taxa de acerto base: %f' % taxa_acerto_base)
+print('Total de testes: %d' % tamanho_teste)
